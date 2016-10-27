7e7348a1a76af094f6d9d706ff8c3dd60858f7c9
Brady Dowling <bradydowling@gmail.com>
2016-10-26 22:02:55 -0700

    Cleanup

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -10,25 +10,21 @@ chrome.storage.sync.get({
     var comments = document.querySelector("#watch-discussion");
 
     if (items.hideFrontpage && frontPageItems) {
-      console.log("should hide the front page");
       frontPageItems.forEach(function(item, key) {
         item.style.display = "none";
       });
     }
     if (items.hideSuggestions && suggestions) {
-      console.log("should hide the suggestions");
       suggestions.forEach(function(suggestion, key) {
         suggestion.style.display = "none";
       });
     }
     if (items.hideAfter && endscreenItems) {
-      console.log("should hide the post video");
       endscreenItems.forEach(function(item, key) {
         item.style.display = "none";
       })
     }
     if (items.hideComments && comments) {
-      console.log("should hide comments");
       comments.style.display = "none";
     }
   });
