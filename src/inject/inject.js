1b4628b2b5df577a11a0479fce8b918907dfd3cc
Brady Dowling <bradydowling@gmail.com>
2016-10-29 19:13:13 +0000

    Hide by default

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,30 +1,30 @@
 chrome.storage.sync.get({
-    hideFrontpage: true,
-    hideSuggestions: true,
-    hideAfter: true,
-    hideComments: true
-  }, function(items) {
-    var frontPageItems = document.querySelectorAll(".individual-feed");
-    var suggestions = document.querySelectorAll(".watch-sidebar-gutter");
-    var endscreenItems = document.querySelector(".ytp-endscreen-content");
-    var comments = document.querySelector("#watch-discussion");
+  hideFrontpage: true,
+  hideSuggestions: true,
+  hideAfter: true,
+  hideComments: true
+}, function(items) {
+  var frontPageItems = document.querySelectorAll(".individual-feed");
+  var suggestions = document.querySelectorAll(".watch-sidebar-gutter");
+  var endscreenItems = document.querySelector(".ytp-endscreen-content");
+  var comments = document.querySelector(".comment-section-renderer");
 
-    if (items.hideFrontpage && frontPageItems) {
-      frontPageItems.forEach(function(item, key) {
-        item.style.display = "none";
-      });
-    }
-    if (items.hideSuggestions && suggestions) {
-      suggestions.forEach(function(suggestion, key) {
-        suggestion.style.display = "none";
-      });
-    }
-    if (items.hideAfter && endscreenItems) {
-      endscreenItems.forEach(function(item, key) {
-        item.style.display = "none";
-      })
-    }
-    if (items.hideComments && comments) {
-      comments.style.display = "none";
-    }
-  });
+  if (!items.hideFrontpage && frontPageItems) {
+    frontPageItems.forEach(function(item, key) {
+      item.style.display = "block";
+    });
+  }
+  if (!items.hideSuggestions && suggestions) {
+    suggestions.forEach(function(suggestion, key) {
+      suggestion.style.display = "block";
+    });
+  }
+  if (!items.hideAfter && endscreenItems) {
+    endscreenItems.forEach(function(item, key) {
+      item.style.display = "block";
+    });
+  }
+  if (!items.hideComments && comments) {
+    comments.style.display = "block";
+  }
+});
